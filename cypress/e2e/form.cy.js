e98f9aaa1e54538dedab059f933b8dfd13478899
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-07 11:12:03 -0600

    test: add form testing

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,20 +1,36 @@
 describe('Form should look and function as it should', () => {
-  it('Should add a posted new order', () => {
+  it('Should have proper error handling and components', () => {
     cy.visit('http://localhost:3000/')
     cy.intercept("GET", 'http://localhost:3001/api/v1/orders', {
       statusCode: 200,
-      fixture: "burritoOrdersPost.json"
-    }).as('orders')
+      fixture: "burritoOrders.json"
+    })
+      .get('input, p').should('be.visible')
+      .get("[name ='beans']").should('be.visible')
+      .get(':nth-child(15)').click()
+      .get('.user-form-warning').should('be.visible')
+  })
+  it('Should be able to add a new order', () => {
+    cy.intercept("GET", 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      fixture: "burritoOrders.json"
+    })
     cy.intercept("POST", 'http://localhost:3001/api/v1/orders', {
       statusCode: 201,
       body: {
         name: 'Hollis', ingredients: [[
           "beans",
-          "sour cream",
-          "jalapeno",
-          "steak"
+          "sour cream"
         ]]
       }
     })
+    cy.get('form').get('input').type('Hollis')
+      .get('[name="beans"]').click().get('[name="sour cream"]').click()
+      .get('p').should('contain', 'Order: beans, sour cream')
+      .get(':nth-child(15)').click()
+      .get('.user-form-warning').should('not.exist')
+      .get('section').invoke('text').should('contain', "Hollis")
+      .get('section').invoke('text').should('contain', "beans")
   })
+
 })
