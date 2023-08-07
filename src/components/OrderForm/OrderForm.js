0aa121333d9caa80cedc06272c50fe7b37e94a89
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-07 10:15:18 -0600

    feat: add error handling for empty form

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
+import './OrderForm.css'
 
 function OrderForm(props) {
   const [name, setName] = useState("");
   const [ingredients, setIngredients] = useState([]);
+  const [alert, setAlert] = useState(false)
 
   function submitOrder(e) {
     e.preventDefault();
@@ -15,14 +17,26 @@ function OrderForm(props) {
     clearInputs();
   }
 
-  useEffect(() => {
-    console.log(ingredients)
-  }, [ingredients])
+  function handleError() {
+    setAlert(true)
+  }
+
+  function handleNonError() {
+    setAlert(false)
+  }
 
   function clearInputs() {
     setName("");
     setIngredients([]);
   };
+
+  function checkFormCompletion() {
+    if (ingredients.length === 0 || name === '') {
+      return false
+    } else {
+      return true
+    }
+  }
 
   const possibleIngredients = [
     "beans",
@@ -45,8 +59,10 @@ function OrderForm(props) {
         name={ingredient}
         value={ingredient}
         onClick={(e) => {
-          e.preventDefault();
-          setIngredients([...ingredients, e.target.value])
+          if (e.target.value !== '') {
+            e.preventDefault();
+            setIngredients([...ingredients, e.target.value])
+          }
         }}
       >
         {ingredient}
@@ -61,12 +77,25 @@ function OrderForm(props) {
         placeholder="Name"
         name="name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => {
+          if (e.target.value !== '') {
+            setName(e.target.value)
+          }
+        }}
       />
       {ingredientButtons}
-      {ingredients.length === 0 && <p>Nothing selected</p>}
+      {ingredients.length === 0 && <p>Nothing Selected</p>}
       {ingredients.length !== 0 && <p>Order: {ingredients.join(', ')}</p>}
-      <button onClick={(e) => submitOrder(e)}>Submit Order</button>
+      <button onClick={(e) => {
+        e.preventDefault();
+        if (checkFormCompletion()) {
+          handleNonError()
+          submitOrder(e)
+        } else {
+          handleError()
+        }
+      }}>Submit Order</button>
+      {alert && <h1 className='user-form-warning'>Please write your name and preferred ingredient(s) above! The form will not submit unless this is complete</h1>}
     </form>
   );
 }
